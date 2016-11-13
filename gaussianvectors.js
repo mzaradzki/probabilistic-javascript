b84db931c86495559f834e3142e7fc96db2ce4ad
mzaradzki <mathieu.zaradzki@gmail.com>
2016-11-13 16:26:57 +0100

    first source version

---
+++ gaussianvectors.js
@@ -0,0 +1,74 @@
+
+
+function Cholesky(A) {
+  // Adapted from this Python version :
+  // https://rosettacode.org/wiki/Cholesky_decomposition#Python2.X_version
+  if (!(numeric.dim(A)[0] === numeric.dim(A)[1])) {
+    throw new Error('Dimension mismatch : '+numeric.dim(A)[0]+' '+numeric.dim(A)[1]);
+  }
+  var size = numeric.dim(A)[0];
+  var L = numeric.mul(A, 0);
+  for (var i=0; i<size; i++)
+  {
+    for (var j=0; j<i+1; j++)
+    {
+      var s = 0;
+      for (var k=0; k<j; k++) {
+        s+= L[i][k] * L[j][k];
+      }
+      if (i == j) {
+        L[i][j] = Math.sqrt(A[i][i] - s);
+      }
+      else {
+        L[i][j] = 1.0 / L[j][j] * (A[i][j] - s);
+      } 
+    }       
+  }
+  return L;
+}
+
+var GaussianLaw = function(means, covariances) {
+
+  if (!(numeric.dim(covariances)[0] === numeric.dim(means)[0])) {
+    throw new Error('Dimension mismatch (0) : '+numeric.dim(covariances)[0]+' '+numeric.dim(means)[0]);
+  }
+  if (!(numeric.dim(covariances)[1] === numeric.dim(means)[0])) {
+    throw new Error('Dimension mismatch (1) : '+numeric.dim(covariances)[1]+' '+numeric.dim(means)[0]);
+  }
+
+  this.dimension = numeric.dim(means);
+  this.means = means;
+  this.covariances = covariances;
+  this.precisions = numeric.inv(covariances);
+  this.cholesky = Cholesky(covariances);
+  this.determinant = numeric.det(covariances);
+  this.determinantsqrt = Math.pow(this.determinant, -0.5);
+  
+  this.determinantlogsqrt = -0.5 * Math.log(this.determinant);
+  this.pdfscale = Math.pow(2*Math.PI, -0.5*this.dimension);
+  this.pdfscalelog = -0.5 * this.dimension * Math.log(2*Math.PI);
+};
+
+GaussianLaw.prototype.pdf = function(X) {
+  var dA = numeric.dotVV(numeric.sub(X,this.means), numeric.dot(this.precisions, numeric.sub(X,this.means)));
+  return this.pdfscale * this.determinantsqrt * Math.exp(-0.5*dA);
+};
+
+GaussianLaw.prototype.logpdf = function(X) {
+  var dA = numeric.dotVV(numeric.sub(X,this.means), numeric.dot(this.precisions, numeric.sub(X,this.means)));
+  return this.pdfscalelog + this.determinantlogsqrt + (-0.5*dA);
+};
+
+GaussianLaw.prototype.simulate = function() {
+  var stdvec = [];
+  for (var i=0; i<this.dimension; i++) {
+    // Box Muller method
+    var u = 1 - Math.random(); // Subtraction to flip [0, 1) to (0, 1].
+    var v = 1 - Math.random();
+    var g = Math.sqrt( -2.0 * Math.log( u ) ) * Math.cos( 2.0 * Math.PI * v );
+    stdvec.push(g);
+  }
+  var covvec = numeric.dot(this.cholesky, stdvec);
+  return numeric.add(this.means, covvec);
+};
+
---
+++ gmm.js
@@ -0,0 +1,115 @@
+
+
+
+var GaussianMixtureModel = function(nbstates, obsdim) {
+	var g = [];
+  var pi = [];
+  for (var i=0; i<nbstates; i++) {
+    var covars = numeric.identity(obsdim);
+    var means = numeric.add(numeric.mul(covars[0], 0), 3+(i-0.5*nbstates)/nbstates); // to have different vectors
+    gaussian = new GaussianLaw(means, covars); // FILL IN MORE ARGUMENTS
+    g.push(gaussian);
+    pi.push(1/nbstates);
+  };
+  this.observationProbabilityCPDs = g;
+  this.stateDistribution = pi;
+
+  this.numberOfStates = nbstates;
+  this.dimensionOfObservations = obsdim;
+};
+
+GaussianMixtureModel.prototype.numberOfStates = 0;
+GaussianMixtureModel.prototype.dimensionOfObservations = 0;
+
+
+GaussianMixtureModel.prototype.simulateStates = function(pathLength, withObservations) {
+
+  var states = [];
+  var observations = [];
+
+  for (var t=0; t<pathLength; t++) {
+    states.push(-1); // dummy assignment to adjust vector length
+    var nextprobas = this.stateDistribution;
+    var r = Math.random();
+    var cumprob = 0;
+    for (var s=0; s<this.numberOfStates; s++) {
+      cumprob+= nextprobas[s];
+      if ((states[t]<0) && (r<cumprob))
+      {
+        states[t] = s;
+      }
+    }
+    if ((states[t]<0) || (states[t]>=this.numberOfStates))
+    {
+      throw new Error('states was not assigned properly');
+    }
+    if (withObservations)
+    {
+      observations.push(this.observationProbabilityCPDs[states[t]].simulate());
+    }
+  }
+  var dico = {states: states};
+  if (withObservations)
+  {
+    dico.observations = observations;
+  }
+  return dico
+};
+
+
+GaussianMixtureModel.prototype._softmax = function(observation) {
+	var nbstates = this.numberOfStates;
+	var probs = [];
+	for (var st=0; st<nbstates; st++) {
+		probs.push(this.observationProbabilityCPDs[st].pdf(observation));
+	}
+	return numeric.div(probs, numeric.sum(probs));
+}
+
+
+GaussianMixtureModel.prototype.fitObservations = function(observations, maxIters) {
+  var iter, t, j, k;
+  var probs;
+  var nbstates = this.numberOfStates;
+  var obsdim = this.dimensionOfObservations;
+  // EM loop
+  for (iter=0; iter<maxIters; iter++) {
+		var probabilities = [];
+		var newStateDistribution = numeric.mul(this.stateDistribution, 0); // null vector of correct dimension
+		// so called Expectation step
+		for (t=0; t<observations.length; t++) {
+			probs = this._softmax(observations[t]);
+			probabilities.push(probs);
+			newStateDistribution = numeric.add(newStateDistribution, probs);
+		};
+		// update
+		this.stateDistribution = numeric.div(newStateDistribution, numeric.sum(newStateDistribution));
+		//
+		for (var st=0; st<nbstates; st++) {
+			var denom = 0;
+			// maximization step : wrt to means
+			var newMeans = numeric.mul(this.observationProbabilityCPDs[st].means, 0); // null vector of correct dimension
+			for (t=0; t<observations.length; t++) {
+				probs = probabilities[t];
+				newMeans = numeric.add(newMeans, numeric.mul(probs[st], observations[t]));
+				denom = denom + probs[st];
+			};
+			newMeans = numeric.div(newMeans, denom);
+			// maximization step : wrt to covariances
+			var newVars = numeric.mul(this.observationProbabilityCPDs[st].covariances, 0); // null matrix of correct dimension
+			for (t=0; t<observations.length; t++) {
+				probs = probabilities[t];
+				for (j=0; j<obsdim; j++) {
+					for (k=0; k<obsdim; k++) {
+						newVars[j][k] = newVars[j][k] + probs[st] * (observations[t][j]-newMeans[j]) * (observations[t][k]-newMeans[k]);
+					}; // k
+				}; // j
+			}; // i
+			newVars = numeric.div(newVars, denom);
+			// update
+			this.observationProbabilityCPDs[st] = new GaussianLaw(newMeans, newVars);
+		}
+	}
+};
+
+
